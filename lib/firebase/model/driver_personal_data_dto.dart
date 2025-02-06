de3ab82410758f45bf3cf6ede6b8911d028ff75f
Wojtas399 <wojtekp26@gmail.com>
2025-02-06 14:24:38 +0100

    DriverPersonalDataDto

---
+++ lib/firebase/model/driver_personal_data_dto.dart
@@ -0,0 +1,31 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'driver_personal_data_dto.g.dart';
+
+@JsonSerializable()
+class DriverPersonalDataDto {
+  @JsonKey(includeFromJson: false, includeToJson: false)
+  final String id;
+  final String name;
+  final String surname;
+
+  DriverPersonalDataDto({
+    this.id = '',
+    required this.name,
+    required this.surname,
+  });
+
+  factory DriverPersonalDataDto.fromFirebase({
+    required String id,
+    required Map<String, dynamic> json,
+  }) {
+    final dto = _$DriverPersonalDataDtoFromJson(json);
+    return DriverPersonalDataDto(
+      id: id,
+      name: dto.name,
+      surname: dto.surname,
+    );
+  }
+
+  Map<String, dynamic> toJson() => _$DriverPersonalDataDtoToJson(this);
+}
