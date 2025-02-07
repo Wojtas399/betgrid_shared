1101dd640fd8799dca5923f0f6766a076e44983b
Wojtas399 <wojtekp26@gmail.com>
2025-02-07 09:08:13 +0100

    Added build.yaml file

--- lib/firebase/firebase_betgrid.dart
+++ lib/firebase/firebase_betgrid.dart
@@ -1,14 +1,12 @@
-import 'package:betgrid_shared/firebase/firebase_dependency_injection.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseBetgrid {
   const FirebaseBetgrid();
 
-  Future<void> initialize({
+  static Future<void> initialize({
     required String name,
     required FirebaseOptions options,
   }) async {
-    configureDependencies();
     await Firebase.initializeApp(
       name: name,
       options: options,
