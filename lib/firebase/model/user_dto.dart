baac6120fabcb6a30ec969cf8077a82d7e8397d6
Wojtas399 <wojtekp26@gmail.com>
2025-02-12 13:20:44 +0100

    Mapping theme enums from string.

--- lib/firebase/model/user_dto.dart
+++ lib/firebase/model/user_dto.dart
@@ -18,22 +18,60 @@ class UserDto {
     return UserDto(
       id: id,
       username: json[UserFields.username],
-      themeMode: json[UserFields.themeMode],
-      themePrimaryColor: json[UserFields.themePrimaryColor],
+      themeMode: _mapThemeModeFromString(json[UserFields.themeMode]),
+      themePrimaryColor: _mapThemePrimaryColorFromString(
+        json[UserFields.themePrimaryColor],
+      ),
     );
   }
 
   Map<String, dynamic> toFirestore() => {
         UserFields.username: username,
-        UserFields.themeMode: themeMode,
-        UserFields.themePrimaryColor: themePrimaryColor,
+        UserFields.themeMode: themeMode.name,
+        UserFields.themePrimaryColor: themePrimaryColor.name,
       };
+
+  static ThemeModeDto _mapThemeModeFromString(String value) {
+    switch (value) {
+      case 'light':
+        return ThemeModeDto.light;
+      case 'dark':
+        return ThemeModeDto.dark;
+      case 'system':
+        return ThemeModeDto.system;
+      default:
+        throw ArgumentError('Invalid theme mode: $value');
+    }
+  }
+
+  static ThemePrimaryColorDto _mapThemePrimaryColorFromString(String value) {
+    switch (value) {
+      case 'red':
+        return ThemePrimaryColorDto.red;
+      case 'pink':
+        return ThemePrimaryColorDto.pink;
+      case 'purple':
+        return ThemePrimaryColorDto.purple;
+      case 'brown':
+        return ThemePrimaryColorDto.brown;
+      case 'orange':
+        return ThemePrimaryColorDto.orange;
+      case 'yellow':
+        return ThemePrimaryColorDto.yellow;
+      case 'green':
+        return ThemePrimaryColorDto.green;
+      case 'blue':
+        return ThemePrimaryColorDto.blue;
+      default:
+        throw ArgumentError('Invalid theme primary color: $value');
+    }
+  }
 }
 
 enum ThemeModeDto { light, dark, system }
 
 enum ThemePrimaryColorDto {
-  defaultRed,
+  red,
   pink,
   purple,
   brown,
