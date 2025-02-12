368efd01ebbfe96edc45747e37802330019d79fd
Wojtas399 <wojtekp26@gmail.com>
2025-02-12 13:04:47 +0100

    New method in FirebaseBetgrid class to use emulators.

--- lib/firebase/firebase_betgrid.dart
+++ lib/firebase/firebase_betgrid.dart
@@ -1,4 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_functions/cloud_functions.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class FirebaseBetgrid {
   const FirebaseBetgrid();
@@ -11,5 +15,12 @@ class FirebaseBetgrid {
       name: name,
       options: options,
     );
+  }
+
+  static void useEmulators() {
+    FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001);
+    FirebaseStorage.instance.useStorageEmulator('localhost', 9199);
   }
 }
