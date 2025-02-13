038b00b3f62a3ca13ad9baf3ec3e1241b952fec2
Wojtas399 <wojtekp26@gmail.com>
2025-02-13 09:22:48 +0100

    Made some params in UserStatsDto nullable

--- lib/firebase/model/user_stats_dto.dart
+++ lib/firebase/model/user_stats_dto.dart
@@ -5,10 +5,10 @@ class UserStatsDto {
   final String userId;
   final int season;
   final double totalPoints;
-  final UserStatsPointsForGpDto bestGpPoints;
-  final UserStatsPointsForGpDto bestQualiPoints;
-  final UserStatsPointsForGpDto bestRacePoints;
-  final List<UserStatsPointsForDriverDto> pointsForDrivers;
+  final UserStatsPointsForGpDto? bestGpPoints;
+  final UserStatsPointsForGpDto? bestQualiPoints;
+  final UserStatsPointsForGpDto? bestRacePoints;
+  final List<UserStatsPointsForDriverDto>? pointsForDrivers;
 
   const UserStatsDto({
     this.userId = '',
@@ -25,48 +25,51 @@ class UserStatsDto {
     required int season,
     required Map<String, dynamic> json,
   }) {
+    final bestGpPointsJson = json[UserStatsFields.bestGpPoints];
+    final bestQualiPointsJson = json[UserStatsFields.bestQualiPoints];
+    final bestRacePointsJson = json[UserStatsFields.bestRacePoints];
+    final pointsForDriversJson = json[UserStatsFields.pointsForDrivers];
+
     return UserStatsDto(
       userId: userId,
       season: season,
       totalPoints: json[UserStatsFields.totalPoints],
-      bestGpPoints: UserStatsPointsForGpDto.fromFirestore(
-        json: json[UserStatsFields.bestGpPoints],
-      ),
-      bestQualiPoints: UserStatsPointsForGpDto.fromFirestore(
-        json: json[UserStatsFields.bestQualiPoints],
-      ),
-      bestRacePoints: UserStatsPointsForGpDto.fromFirestore(
-        json: json[UserStatsFields.bestRacePoints],
-      ),
-      pointsForDrivers: _mapPointsForDriversFromFirestore(
-        json[UserStatsFields.pointsForDrivers],
-      ),
+      bestGpPoints: bestGpPointsJson != null
+          ? UserStatsPointsForGpDto.fromFirestore(json: bestGpPointsJson)
+          : null,
+      bestQualiPoints: bestQualiPointsJson != null
+          ? UserStatsPointsForGpDto.fromFirestore(json: bestQualiPointsJson)
+          : null,
+      bestRacePoints: bestRacePointsJson != null
+          ? UserStatsPointsForGpDto.fromFirestore(json: bestRacePointsJson)
+          : null,
+      pointsForDrivers: pointsForDriversJson != null
+          ? _mapPointsForDriversFromFirestore(pointsForDriversJson)
+          : null,
     );
   }
 
   Map<String, dynamic> toFirestore() => {
         UserStatsFields.totalPoints: totalPoints,
-        UserStatsFields.bestGpPoints: bestGpPoints.toFirestore(),
-        UserStatsFields.bestQualiPoints: bestQualiPoints.toFirestore(),
-        UserStatsFields.bestRacePoints: bestRacePoints.toFirestore(),
+        UserStatsFields.bestGpPoints: bestGpPoints?.toFirestore(),
+        UserStatsFields.bestQualiPoints: bestQualiPoints?.toFirestore(),
+        UserStatsFields.bestRacePoints: bestRacePoints?.toFirestore(),
         UserStatsFields.pointsForDrivers: _mapPointsForDriversToFirestore(
           pointsForDrivers,
         ),
       };
 
   static List<UserStatsPointsForDriverDto> _mapPointsForDriversFromFirestore(
-    List<dynamic>? jsonsList,
+    List<dynamic> jsonsList,
   ) =>
-      jsonsList != null
-          ? jsonsList
-              .map((e) => UserStatsPointsForDriverDto.fromFirestore(json: e))
-              .toList()
-          : [];
+      jsonsList
+          .map((e) => UserStatsPointsForDriverDto.fromFirestore(json: e))
+          .toList();
 
-  static List<Map<String, dynamic>> _mapPointsForDriversToFirestore(
-    List<UserStatsPointsForDriverDto> dtos,
+  static List<Map<String, dynamic>>? _mapPointsForDriversToFirestore(
+    List<UserStatsPointsForDriverDto>? dtos,
   ) =>
-      dtos.map((e) => e.toFirestore()).toList();
+      dtos?.map((e) => e.toFirestore()).toList();
 }
 
 class UserStatsFields {
