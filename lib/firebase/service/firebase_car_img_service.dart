b58abbbf3071d4b75b05e1968f976cda67f4e94d
Wojtas399 <wojtekp26@gmail.com>
2025-04-05 20:55:20 +0200

    Improved method to fetch ulr of car image.

--- lib/firebase/service/firebase_car_img_service.dart
+++ lib/firebase/service/firebase_car_img_service.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
 class FirebaseCarImgService {
@@ -8,23 +7,16 @@ class FirebaseCarImgService {
     required int season,
     required String imgFileName,
   }) async {
-    return await _doesCarImgExists(season: season, imgFileName: imgFileName)
-        ? await FirebaseStorage.instance
-            .ref('${_storagePath(season)}/$imgFileName')
-            .getDownloadURL()
-        : null;
-  }
+    try {
+      return await FirebaseStorage.instance
+          .ref('${_storagePath(season)}/$imgFileName')
+          .getDownloadURL();
+    } on FirebaseException catch (e) {
+      if (e.code == 'object-not-found') {
+        return null;
+      }
 
-  Future<bool> _doesCarImgExists({
-    required int season,
-    required String imgFileName,
-  }) async {
-    final allCarImgs =
-        await FirebaseStorage.instance.ref(_storagePath(season)).listAll();
-
-    return allCarImgs.items.firstWhereOrNull(
-          (Reference reference) => reference.fullPath.contains(imgFileName),
-        ) !=
-        null;
+      rethrow;
+    }
   }
 }
